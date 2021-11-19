2d12ef6afe0216925799a660b293d2d61e1cde65
Miguel Angel Sanchez Solis <ing.miguels@gmail.com>
2021-11-19 08:59:37 -0600

    Added controller

---
+++ src/main/java/com/itj/workshop/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.itj.workshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello world";
+    }
+}
